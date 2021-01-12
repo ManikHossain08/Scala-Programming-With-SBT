de7b5f5b66fea37355ee50fd6a85743e22888c91
ManikHossain08 <manik083017@gmail.com>
2021-01-12 09:07:58 -0500

    scala nested functions

---
+++ scala-playground/src/main/scala/ca/basic/scala/bigdata/Scala_NestedFuntion.scala
@@ -0,0 +1,27 @@
+package ca.basic.scala.bigdata
+
+object Scala_NestedFuntion extends App {
+
+  /** if the input list has any odd number
+   *
+   * Exists: if there exists any member in the collection which the condition evaluates to true
+   **/
+
+  def hasOdd(list: List[Int]): Boolean = {
+
+    def isOdd(in: Int): Boolean = in % 2 == 1
+    list.exists(isOdd) // returning boolean type
+  }
+
+  /**
+   * this isOdd() method is not accessible from outside of the method of hasOdd()
+   * hence, this bellow statement is not acceptable and commented it below.
+   */
+  // def hasOdd(in: List[Int]): Boolean = in.filter(isOdd).nonEmpty
+
+  println("==== Test hasOdd")
+  println("Has Odd: "+ hasOdd(List(2, 3, 8)))
+  println("Has Odd: "+hasOdd(List(2, 4, 8)))
+
+
+}

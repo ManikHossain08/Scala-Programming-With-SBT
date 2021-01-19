b58f01009dff4a455875263038d2fda48af9a28f
ManikHossain08 <manik083017@gmail.com>
2021-01-19 11:00:21 -0500

    Scala othr classes like object, class and case class

---
+++ scala-playground/src/main/scala/ca/basic/scala/bigdata/ScalaUseOfOtherClasses.scala
@@ -0,0 +1,34 @@
+package ca.basic.scala.bigdata
+
+object ScalaUseOfOtherClasses extends App {
+  println(ObjectWithFunctions.myFunction("Laval"))
+  println(new ClassWithFunctions().myFunction("Toronto"))
+  println(CaseClassWithFunctions().myFunction("Null"))
+}
+
+object ObjectWithFunctions {
+  private val lookUpTable = Map(
+    ("Montreal", "QC"),
+    ("Toronto", "ON"),
+    ("Laval", "QC")
+  )
+  def myFunction(city: String) =  lookUpTable(city)
+}
+
+class ClassWithFunctions(){
+  private val lookUpTable = Map(
+    ("Montreal", "QC"),
+    ("Toronto", "ON"),
+    ("Laval", "QC")
+  )
+  def myFunction(city: String) =  lookUpTable(city)
+}
+
+case class CaseClassWithFunctions(){
+  private val lookUpTable = Map(
+    ("Montreal", "QC"),
+    ("Toronto", "ON"),
+    ("Laval", "QC")
+  )
+  def myFunction(city: String) =  if(lookUpTable.contains(city)) lookUpTable(city) else null
+}
